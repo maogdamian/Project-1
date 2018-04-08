56dbb0e9931f62f4904b921bd0c388645c6ef3ed
maogdamian <maogdamian@gmail.com>
2018-04-08 11:40:29 -0700

    created html files and sudo coding

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+//Theater Tickets App - Project 1
+
+//Contributers
+//Scott
+//Jared
+//Nathan
+//Damian
+
+//Set Firebase Database
+
+//Set Ajax calls
+
+//Create Variables
+
+var name = "";
+var email = "";
+var phone = "";
+var month = "";
+var moviePoster = "";
+var movieTitle = "";
+var movieReleaseDate = "";
+var movieRadio = "";
+var preferredTheater = "";
+var preferredExperience = "";
+
+//Create an array of Movie Images from NYT json response
+
+//Create an array of Movie Titles from NYT json response
+
+//Create an array of Movie Radio buttons to accompany movie title
+
+//Create PUSH to build database when user creates account
+
+//Create PULL to build users account page
+
+//Create logic to set preferred theater
+
+//Create logic to set preferred theater experience
+
+
